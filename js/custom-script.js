36193f0dbefc899e554c9e32b185160fbb1aca90
Kelvin Lee <orangejuiceboy@gmail.com>
2016-01-01 16:31:58 -0800

    Organize HTML; .ready() fills in SO + GH links; remove inline style.

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,19 +1,32 @@
-function toggleNonWork(){
-	var button = document.getElementById("non-work");
-	var current = button.innerHTML;
+$(document).ready(function(){
 
-	if (current == primaryNonWorkString){
-		button.innerHTML = secondaryNonWorkString;
-		secondaryDisplay.style.display = "block";
-		primaryDisplay.style.display = "none";
-	}
+    var mySO = "http://stackoverflow.com/users/5455994/"
+             + "kelvin-lee?tab=profile";
+    var myGH= "http://github.com/kelvin-lee";
+    document.getElementById("mySO").href = mySO;
+    document.getElementById("myGH").href = myGH;
 
-	if (current == secondaryNonWorkString){
-		button.innerHTML = primaryNonWorkString;
-		primaryDisplay.style.display = "block"
-		secondaryDisplay.style.display = "none";
-	}
+});
 
-	/* EXCEPTION HANDLER should go here */
-}
+function toggleNonWork()
+{
+    var button = document.getElementById("non-work");
+    var current = button.innerHTML;
+    
+    if (current == primaryNonWorkString)
+    {
+        button.innerHTML = secondaryNonWorkString;
+	secondaryDisplay.style.display = "block";
+	primaryDisplay.style.display = "none";
+    }
+
+    if (current == secondaryNonWorkString)
+    {
+        button.innerHTML = primaryNonWorkString;
+	primaryDisplay.style.display = "block"
+	secondaryDisplay.style.display = "none";
+    }
+
+    /* EXCEPTION HANDLER should go here */
+}//toggleNonWork
 
