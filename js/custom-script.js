ef909028037bf2e3d64e721f655e566db58ca822
Kelvin Lee <orangejuiceboy@gmail.com>
2016-01-04 20:07:39 -0800

    Create pixel art divs + basic mouseover handlers for interaction.

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,11 +1,37 @@
 $(document).ready(function(){
 
-    var mySO = "http://stackoverflow.com/users/5455994/"
-             + "kelvin-lee?tab=profile";
-    var myGH= "http://github.com/kelvin-lee";
-    document.getElementById("mySO").href = mySO;
-    document.getElementById("myGH").href = myGH;
+    $('#tog').click(function(){
+        $(".mypx").html('');
+    });
 
+    $('#SOhover').mouseover(function()
+    {
+    $(".outer").css("background-color", "#888888");
+    $(".top").css("background-color", "inherit");
+    $(" #2,  #3,  #4,  #5").css("background-color", "#FFA500");
+    //$(" #8,  #9, #10, #11").css("background-color", "#E0B050");
+    $(" #8,  #9, #10, #11").css("background-color", "inherit");
+    $("#14, #15, #16, #17").css("background-color", "#C58950");
+    $("#20, #21, #22, #23").css("background-color", "#787878");
+    $("#26, #27, #28, #29").css("background-color", "#505050");
+    
+    
+    });
+
+    $('#cornerhover').mouseover(function()
+    {
+console.log('#cornerhover');
+    });
+
+    $('#boxhover').mouseover(function()
+    {
+console.log('#boxhover');
+    });
+    
+    $('#innerhover').mouseover(function()
+    {
+console.log('#innerhover');
+    });
 });
 
 function toggleNonWork()
