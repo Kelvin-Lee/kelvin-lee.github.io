0aa9101d257601904fb4c1b056fa195c3c281718
Kelvin Lee <orangejuiceboy@gmail.com>
2016-01-06 20:11:21 -0800

    Finish SO art (hardcode for 15x15)

--- js/custom-script.js
+++ js/custom-script.js
@@ -6,17 +6,34 @@ $(document).ready(function(){
 
     $('#SOhover').mouseover(function()
     {
-    $(".outer").css("background-color", "#888888");
-    $(".top").css("background-color", "inherit");
-    $(" #2,  #3,  #4,  #5").css("background-color", "#FFA500");
-    //$(" #8,  #9, #10, #11").css("background-color", "#E0B050");
-    $(" #8,  #9, #10, #11").css("background-color", "inherit");
-    $("#14, #15, #16, #17").css("background-color", "#C58950");
-    $("#20, #21, #22, #23").css("background-color", "#787878");
-    $("#26, #27, #28, #29").css("background-color", "#505050");
+
+	//$(".inner").css("background-color", "#000000");
+
+        // Grey stack container
+	$("#121, #135, #136, #150, #151, #165, #166, #180, #181, #195, #196, #210").css("background-color", "#444444");
+	$(".bottom").css("background-color", "#444444");
+
+        // Bottom stack layer
+        $(/*"#168, #169, #170, #171, #172, #173, #174, #175, #176, #177, #178"*/
+      /*+*/ " #183, #184, #185, #186, #187, #188, #189, #190, #191, #192, #193").css("background-color","orange");
+
+        // Second-from-bottom stack layer
+	$("#153, #154, #155, #156, #157, #158, #159, #160, #161, #162, #163").css("background-color", "orange");
+
+        // Third-from-bottom stack layer
+        $("#123, #124, #125, #126, #127"/* "#108, #109, #110, #111, #112"*/ + ", #128, #129, #130, #131, #132, #133"
+     /* + ", #138, #139, #140, #141, #142, #143, #144, #145, #146, #147, #148"*/).css("background-color", "orange");
+
+
+      // First (from bottom) tilted stack layer
+      $("#63, #64, #65, #66" + ", #82, #83, #84, #85" + ",#101, #102, #103").css("background-color", "orange");
+
+      // Second (from bottom) tilted stack layer
+      $("#4, #5" + ", #21, #22" + ", #38, #39" + ", #55, #56" +", #72, #73").css("background-color", "orange");
     
     
     });
+
 
     $('#cornerhover').mouseover(function()
     {
@@ -46,7 +63,8 @@ function makeSquare(n)
 	var isRSide = (i % n == 0)
 
         var strB = ""
-	var strC  = "' id = '" + i + "'>" + i + "</div>"
+	//var strC  = "' id = '" + i + "'>" +  i + "</div>"
+	var strC  = "' id = '" + i + "'>" + "</div>"
 
     	if (isTop || isBottom || isLSide || isRSide)
 	{
@@ -70,6 +88,13 @@ function makeSquare(n)
 
 
         $('#bigbox').append(strA + strB + strC);
+   
+        var strD = '#' + i;
+        $(strD).click(function()
+        {
+           $(strD).css("background-color", "orange");
+        });
+	
     }
 
     var pixdim = 25;
